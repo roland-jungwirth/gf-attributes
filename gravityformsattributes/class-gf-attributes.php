7a7a389ef8077ce1497ca3c76e02336e3d304779
juro <roland@top-node.com>
2020-05-12 10:46:26 +0200

    Add files via upload

---
+++ gravityformsattributes/class-gf-attributes.php
@@ -0,0 +1,193 @@
+<?php
+
+//------------------------------------------
+
+// don't load directly
+	if ( ! defined( 'ABSPATH' ) ) {
+		die();
+	}
+
+	GFForms::include_addon_framework();
+
+	class GFAttributes extends GFAddOn {
+
+		protected $_version = GF_ATTRIBUTES_VERSION;
+		protected $_min_gravityforms_version = '2.0';
+		protected $_slug = 'gravityformsattributes';
+		protected $_path = 'gravityformsattributes/attributes.php';
+		protected $_full_path = __FILE__;
+		protected $_url = 'http://www.gravityforms.com';
+		protected $_title = 'Gravity Forms Attributes Add-On';
+		protected $_short_title = 'Attributes';
+		protected $_enable_rg_autoupgrade = TRUE;
+
+		/**
+		 * @var object $_instance If available, contains an instance of this class.
+		 */
+		private static $_instance = NULL;
+
+		/**
+		 * Returns an instance of this class, and stores it in the $_instance property.
+		 *
+		 * @return object $_instance An instance of this class.
+		 */
+		public static function get_instance() {
+			if ( self::$_instance == NULL ) {
+				self::$_instance = new GFAttributes();
+			}
+
+			return self::$_instance;
+		}
+
+		private function __clone() {
+		} /* do nothing */
+
+		/**
+		 * Include the field early so it is available when entry exports are being performed.
+		 */
+		public function pre_init() {
+			parent::pre_init();
+
+			if ( $this->is_gravityforms_supported() && class_exists( 'GF_Field' ) ) {
+				require_once( 'includes/class-gf-attributes.php' );
+
+				add_filter( 'gform_export_field_value', array( $this, 'export_field_value' ), 10, 4 );
+			}
+		}
+
+		/**
+		 * Handles hooks and loading of language files.
+		 */
+		public function init() {
+			// add a special class to likert fields so we can identify them later
+			add_action( 'gform_field_css_class', array( $this, 'add_custom_class' ), 10, 3 );
+
+			parent::init();
+		}
+
+		public function init_admin() {
+			parent::init_admin();
+		}
+
+
+		// # SCRIPTS & STYLES -----------------------------------------------------------------------------------------------
+
+		/**
+		 * Return the scripts which should be enqueued.
+		 *
+		 * @return array
+		 */
+		public function scripts() {
+			$scripts = array(
+				array(
+					'handle'   => 'gf_attributes_form_editor_js',
+					'src'      => $this->get_base_url() . '/js/gf_attributes_form_editor.js',
+					'version'  => $this->_version,
+					'deps'     => array( 'jquery' ),
+					'callback' => array( $this, 'localize_scripts' ),
+					'enqueue'  => array(
+						array( 'admin_page' => array( 'form_editor' ) ),
+					),
+				),
+				array(
+					'handle'  => 'gf_attributes_js',
+					'src'     => $this->get_base_url() . '/js/gf_attributes.js',
+					'version' => $this->_version,
+					'enqueue' => array(
+						array( 'field_types' => array( 'attributes' ) ),
+					),
+				),
+			);
+
+			return array_merge( parent::scripts(), $scripts );
+		}
+
+		/**
+		 * Include my_styles.css when the form contains a 'simple' type field.
+		 *
+		 * @return array
+		 */
+		public function styles() {
+			$styles = array(
+				array(
+					'handle'  => 'gf_attributes_form_editor_css',
+					'src'     => $this->get_base_url() . '/css/gf_attributes_form_editor.css',
+					'version' => $this->_version,
+					'enqueue' => array(
+						array( 'admin_page' => array( 'form_editor' ) ),
+					),
+				),
+				array(
+					'handle'  => 'gf_attributes_css',
+					'src'     => $this->get_base_url() . '/css/gf_attributes.css',
+					'version' => $this->_version,
+					'enqueue' => array(
+						array( 'field_types' => array( 'attributes' ) ),
+					),
+				),
+			);
+
+			return array_merge( parent::styles(), $styles );
+		}
+
+		/**
+		 * Localize the strings used by the scripts.
+		 */
+		public function localize_scripts() {
+			// Get current page protocol
+			$protocol = isset( $_SERVER['HTTPS'] ) ? 'https://' : 'http://';
+			// Output admin-ajax.php URL with same protocol as current page
+			$params = array(
+				'ajaxurl'   => admin_url( 'admin-ajax.php', $protocol ),
+				'imagesUrl' => $this->get_base_url() . '/images',
+				'strings'   => array(
+					'untitledAttributesField' => wp_strip_all_tags( __( 'Untitled Attributes Field', 'gravityformsattributes' ) ),
+				),
+			);
+			wp_localize_script( 'gsurvey_form_editor_js', 'gsurveyVars', $params );
+
+			//localize strings for the js file
+			$strings = array(
+				'firstChoice'   => wp_strip_all_tags( __( 'First attribute', 'gravityformssurvey' ) ),
+				'secondChoice'  => wp_strip_all_tags( __( 'Second attribute', 'gravityformssurvey' ) ),
+				'thirdChoice'   => wp_strip_all_tags( __( 'Third attribute', 'gravityformssurvey' ) ),
+				'fourthChoice'  => wp_strip_all_tags( __( 'Fourth attribute', 'gravityformssurvey' ) ),
+				'fifthChoice'   => wp_strip_all_tags( __( 'Fifth attribute', 'gravityformssurvey' ) ),
+				'dragToReOrder' => wp_strip_all_tags( __( 'Drag to re-order', 'gravityformssurvey' ) ),
+				'addAnotherRow' => wp_strip_all_tags( __( 'Add another row', 'gravityformssurvey' ) ),
+				'removeThisRow' => wp_strip_all_tags( __( 'Remove this row', 'gravityformssurvey' ) ),
+
+			);
+			wp_localize_script( 'gf_attributes_form_editor_js', 'gfAttributesStrings', $strings );
+		}
+
+		/**
+		 * Add the gsurvey-field class to the Survey field.
+		 *
+		 * @param string $classes The CSS classes to be filtered, separated by empty spaces.
+		 * @param GF_Field $field The field currently being processed.
+		 * @param array $form The form currently being processed.
+		 *
+		 * @return string
+		 */
+		public function add_custom_class( $classes, $field, $form ) {
+			$classes .= ' gf-attributes-field ';
+
+			return $classes;
+		}
+
+		/**
+		 * Format the Survey field values on the entry detail page so they use the choice text instead of values.
+		 *
+		 * @param string|array $value The field value.
+		 * @param GF_Field $field The field currently being processed.
+		 * @param array $entry The entry object currently being processed.
+		 * @param array $form The form object currently being processed.
+		 *
+		 * @return string|array
+		 */
+		public function entry_field_value( $value, $field, $entry, $form ) {
+
+			return ! rgblank( $value ) ? $this->maybe_format_field_values( $value, $field ) : $value;
+		}
+	}
---
+++ gravityformsattributes/includes/class-gf-attributes.php
@@ -0,0 +1,155 @@
+<?php
+
+	if ( ! class_exists( 'GFForms' ) ) {
+		die();
+	}
+
+	class GF_Attributes_Field extends GF_Field {
+
+		/**
+		 * @var string $type The field type.
+		 */
+		public $type = 'attributes';
+
+		private $attributes_default = array(
+			array( 'id' => 'zf_bereich_1', 'label' => 'Aus- / Weiterbildung' ),
+			array( 'id' => 'zf_bereich_2', 'label' => 'Berufliche Situation' ),
+			array( 'id' => 'zf_bereich_3', 'label' => 'Finanzielle Situation' ),
+			array( 'id' => 'zf_bereich_4', 'label' => 'Gesundheit' ),
+			array( 'id' => 'zf_bereich_5', 'label' => 'Familie' ),
+			array( 'id' => 'zf_bereich_6', 'label' => 'Kinder' ),
+			array( 'id' => 'zf_bereich_7', 'label' => 'Freunde' ),
+			array( 'id' => 'zf_bereich_8', 'label' => 'Beziehung' ),
+			array( 'id' => 'zf_bereich_9', 'label' => 'Wohnsituation' ),
+			array( 'id' => 'zf_bereich_11', 'label' => 'Entspannung' ),
+			array( 'id' => 'zf_bereich_12', 'label' => 'Lebensfreude' ),
+			array( 'id' => 'zf_bereich_13', 'label' => 'Persönliche Entwicklung' ),
+		);
+
+		public function set_attributes( $attributes ) {
+			$this->attributes_default = $attributes;
+		}
+
+		public function get_attributes() {
+			return $this->attributes_default;
+		}
+
+		public function get_form_editor_field_title() {
+			return esc_attr__( 'Attributes', 'attributes' );
+		}
+
+		/**
+		 * The settings which should be available on the field in the form editor.
+		 *
+		 * @return array
+		 */
+		function get_form_editor_field_settings() {
+			return array(
+				'conditional_logic_field_setting',
+				'prepopulate_field_setting',
+				'error_message_setting',
+				'label_setting',
+				'label_placement_setting',
+				'admin_label_setting',
+				'rules_setting',
+				'visibility_setting',
+				'description_setting',
+				'css_class_setting',
+			);
+		}
+
+		public function is_conditional_logic_supported() {
+			return TRUE;
+		}
+
+		public function get_field_input( $form, $value = '', $entry = NULL ) {
+			$form_id        = absint( $form['id'] );
+			$is_admin       = $this->is_entry_detail() || $this->is_form_editor();
+			$is_form_editor = $this->is_form_editor();
+
+			$id            = $this->id;
+			$field_id      = $form_id == 0 ? "gf-attributes-$id" : "gf-attributes-$form_id-$id";
+			$disabled_text = $is_form_editor ? 'disabled="disabled"' : '';
+
+			$hidden_input     = sprintf( "<input type='hidden' id='%s-hidden' name='input_%d' />", $field_id, $this->id );
+			$points_available = sprintf( "<p class='gf_attributes_result'>Sie können noch <span id='gf-attributes-remainder-%s-%s'>10</span> Punkte vergeben.</p>", $form_id, $this->id );
+
+			if ( is_admin() ) {
+				return sprintf( 'Dieses Feld wird im Code mit Inhalten bef&uuml;llt.' );
+			} else {
+				return sprintf(
+					"<div class='ginput_container ginput_container_attributes'>%s<ul class='gf-attributes-options' id='%s'>%s</ul>%s</div>",
+					$points_available,
+					$field_id,
+					$this->get_attribute_choices( $value, $disabled_text, $form_id ),
+					$hidden_input
+				);
+			}
+		}
+
+		/**
+		 * Returns the markup for the attribute choices.
+		 *
+		 * @param string|array $value The field value. From default/dynamic population, $_POST, or a resumed incomplete submission.
+		 * @param string       $disabled_text The input disabled attribute when in the form editor or entry detail page.
+		 * @param int          $form_id The ID of the current form.
+		 *
+		 * @return string
+		 */
+		public function get_attribute_choices( $value, $disabled_text, $form_id ) {
+			$attributes      = '';
+			$is_entry_detail = $this->is_entry_detail();
+			$is_form_editor  = $this->is_form_editor();
+			$is_admin        = $is_entry_detail || $is_form_editor;
+
+			$attribute_number = 1;
+
+			// Loop through field choices.
+			foreach ( $this->attributes_default as $attribute ) {
+				// Hack to skip numbers ending in 0, so that 5.1 doesn't conflict with 5.10.
+
+				if ( $is_entry_detail || $is_form_editor || $form_id == 0 ) {
+					$id = $this->id . '_' . $attribute_number ++;
+				} else {
+					$id = $form_id . '_' . $this->id . '_' . $attribute_number ++;
+				}
+
+				$tabindex        = $this->get_tabindex();
+				$attribute_id    = $attribute['id'];
+				$input_id        = $this->id . '_' . $attribute_id;
+				$attribute_value = $attribute['value'];
+
+				$attribute_markup = "<li class='gf_attributes_{$id}'>
+					<input name='attribute_{$input_id}' type='text' value='{$attribute_value}' id='{$input_id}' class='square' {$tabindex} {$disabled_text} />
+					<label for='attribute_{$id}' id='label_{$input_id}'>{$attribute['label']}</label>
+				</li>";
+
+				/**
+				 * Override the default choice markup used when rendering radio button, checkbox and drop down type fields.
+				 *
+				 * @param string $attribute_markup The string containing the choice markup to be filtered.
+				 * @param array  $attribute An associative array containing the choice properties.
+				 * @param object $field The field currently being proce ssed.
+				 * @param string $value The value to be selected if the field is being populated.
+				 *
+				 * @since 1.9.6
+				 *
+				 */
+				$attributes .= gf_apply_filters( array( 'gform_field_choice_markup_pre_render', $this->formId, $this->id ), $attribute_markup, $attribute, $this, $value );
+			}
+
+			/**
+			 * Modify the checkbox items before they are added to the checkbox list.
+			 *
+			 * @param string $attributes The string containing the choices to be filtered.
+			 * @param object $field Ahe field currently being processed.
+			 *
+			 * @since Unknown
+			 *
+			 */
+			return gf_apply_filters( array( 'gform_field_choices', $this->formId, $this->id ), $attributes, $this );
+		}
+
+	}
+
+	GF_Fields::register( new GF_Attributes_Field() );
